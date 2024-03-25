2120ac7e8bc031d3c5203b3d189e5b196189c651
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-03-25 20:31:30 +0200

    Add Old Icelandic dictionary builder

---
+++ norsedictionaries/src/main/kotlin/io/github/stscoundrel/norsedictionaries/oldicelandic/DictionaryBuilder.kt
@@ -0,0 +1,56 @@
+package io.github.stscoundrel.norsedictionaries.oldicelandic
+
+
+import io.github.stscoundrel.norsedictionaries.connectToSQLiteDatabase
+import io.github.stscoundrel.norsedictionaries.readResourceFile
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import java.sql.Connection
+
+private const val jsonPath = "old-icelandic-dictionary.json"
+private const val databasePath = "src/main/resources/old-icelandic-dictionary.db"
+
+
+fun createOldIcelandicDatabase() {
+    val entries = getDictionaryEntries()
+
+    val connection = connectToSQLiteDatabase(databasePath)
+
+    createDatabaseTable(connection)
+    createDatabaseEntries(connection, entries)
+
+    connection.close()
+}
+
+private fun getDictionaryEntries(): List<OldIcelandicDictionaryEntry> {
+    val jsonContent = readResourceFile(jsonPath)
+    return Json.decodeFromString<List<OldIcelandicDictionaryEntry>>(jsonContent)
+}
+
+private fun createDatabaseTable(connection: Connection) {
+    val statement = connection.createStatement()
+    val sql = """
+        CREATE TABLE IF NOT EXISTS old_icelandic (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            headword TEXT NOT NULL,
+            definitions TEXT NOT NULL
+        );
+    """.trimIndent()
+    statement.executeUpdate(sql)
+    statement.close()
+}
+
+private fun createDatabaseEntries(connection: Connection, entries: List<OldIcelandicDictionaryEntry>) {
+    val sql = "INSERT INTO old_icelandic (headword, definitions) VALUES (?, ?)"
+    val preparedStatement = connection.prepareStatement(sql)
+
+    for (entry in entries) {
+        val definitionsString = entry.definitions.joinToString(separator = "\n")
+        preparedStatement.setString(1, entry.headword)
+        preparedStatement.setString(2, definitionsString)
+        preparedStatement.addBatch()
+    }
+
+    preparedStatement.executeBatch()
+    preparedStatement.close()
+}
